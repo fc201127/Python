e946c09f74fdd5e9c96771958efa2afc814c86c2
BEN <fc1127@gmail.com>
2025-01-21 17:53:00 +0800

    使用者在按下sqrt之後，就會跑出一個結果，當使用者按下1-9其中一個數字時，剛剛sqrt的結果就會不見，重新來過

--- calc.py
+++ calc.py
@@ -8,6 +8,8 @@ import math
 # 可以理解成: Calculator "is a" tk.Frame
 # 
 class Calculator(tk.Frame):
+    shouldReset = True
+
     def __init__(self): # 所有 class 都要有一個 __init__(self) 的 func.
                         # 可以理解成, Calculator "has a" __init__ func.
         tk.Frame.__init__(self)
@@ -24,7 +26,8 @@ class Calculator(tk.Frame):
                         command=self.clickBtnNum1, height=1, width=2, font=f2)
         self.btnNum2 = tk.Button(self, text="2",
                         command=self.clickBtnNum2, height=1, width=2, font=f2)
-        self.btnNum3 = tk.Button(self, text="3", command=self.clickBtnNum3, height=1, width=2, font=f2)
+        self.btnNum3 = tk.Button(self, text="3",
+                        command=self.clickBtnNum3, height=1, width=2, font=f2)
         self.btnNum4 = tk.Button(self, text="4",
                         command=self.clickBtnNum4, height=1, width=2, font=f2)
         self.btnNum5 = tk.Button(self, text="5",
@@ -66,33 +69,83 @@ class Calculator(tk.Frame):
 
         
     def clickBtnNum1(self):
-        self.lblNum.configure(text=self.lblNum.cget("text") + "1")
+        if self.shouldReset == True:
+            self.lblNum.configure(text= "1")
+            self.shouldReset = False
+        else:
+            self.lblNum.configure(text=self.lblNum.cget("text") + "1")
+
     def clickBtnNum2(self):
-        self.lblNum.configure(text=self.lblNum.cget("text") + "2")
+        if self.shouldReset == True:
+            self.lblNum.configure(text= "2")
+            self.shouldReset = False
+        else:
+            self.lblNum.configure(text=self.lblNum.cget("text") + "2")
+
     def clickBtnNum3(self):
-        self.lblNum.configure(text=self.lblNum.cget("text") + "3")
+        if self.shouldReset == True:
+            self.lblNum.configure(text= "3")
+            self.shouldReset = False
+        else:
+            self.lblNum.configure(text=self.lblNum.cget("text") + "3")
+
     def clickBtnNum4(self):
-        self.lblNum.configure(text=self.lblNum.cget("text") + "4")
+        if self.shouldReset == True:
+            self.lblNum.configure(text= "4")
+            self.shouldReset = False
+        else:
+            self.lblNum.configure(text=self.lblNum.cget("text") + "4")
+
     def clickBtnNum5(self):
-        self.lblNum.configure(text=self.lblNum.cget("text") + "5")
+        if self.shouldReset == True:
+            self.lblNum.configure(text= "5")
+            self.shouldReset = False
+        else:
+            self.lblNum.configure(text=self.lblNum.cget("text") + "5")
     def clickBtnNum6(self):
-        self.lblNum.configure(text=self.lblNum.cget("text") + "6")
+        if self.shouldReset == True:
+            self.lblNum.configure(text= "6")
+            self.shouldReset = False
+        else:
+            self.lblNum.configure(text=self.lblNum.cget("text") + "6")
+
     def clickBtnNum7(self):
-        self.lblNum.configure(text=self.lblNum.cget("text") + "7")
+        if self.shouldReset == True:
+            self.lblNum.configure(text= "7")
+            self.shouldReset = False
+        else:
+            self.lblNum.configure(text=self.lblNum.cget("text") + "7")
+
     def clickBtnNum8(self):
-        self.lblNum.configure(text=self.lblNum.cget("text") + "8")
+        if self.shouldReset == True:
+            self.lblNum.configure(text= "8")
+            self.shouldReset = False
+        else:
+            self.lblNum.configure(text=self.lblNum.cget("text") + "8")
+
     def clickBtnNum9(self):
-        self.lblNum.configure(text=self.lblNum.cget("text") + "9")
+        if self.shouldReset == True:
+            self.lblNum.configure(text= "9")
+            self.shouldReset = False
+        else:
+            self.lblNum.configure(text=self.lblNum.cget("text") + "9")
+
     def clickBtnNum0(self):
-        self.lblNum.configure(text=self.lblNum.cget("text") + "0")
+        if self.shouldReset == True:
+            self.lblNum.configure(text= "0")
+            self.shouldReset = False
+        else:
+            self.lblNum.configure(text=self.lblNum.cget("text") + "0")
+
     def clickBtnNumSqrt(self):
         v = str(round(math.sqrt(float(self.lblNum.cget("text"))), 2))
         self.lblNum.configure(text=v)
+        self.shouldReset = True
 
 def main():
     cal = Calculator()
-    cal.master.title("My Calculator v1.2")
+    cal.master.title("My Calculator v1.3")
     cal.mainloop()
 
-if __name == "__main__":
+if __name__ == "__main__":
     main()
